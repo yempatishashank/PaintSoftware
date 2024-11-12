6023728aa177bd144e06e078f29b144196563a73
Yagnesh <143516503+Yagnesh2910@users.noreply.github.com>
2024-11-12 22:51:28 +0530

    Update DrawingContext.java

--- src/main/java/DrawingContext.java
+++ src/main/java/DrawingContext.java
@@ -2,15 +2,12 @@ public class DrawingContext {
     public void setPoint() {
         System.out.println("Setting point in drawing context");
     }
-
     public void clearScreen() {
         System.out.println("Clearing screen");
     }
-
     public int getVerticalSize() {
         return 100; 
     }
-
     public int getHorizontalSize() {
         return 200; 
     }
