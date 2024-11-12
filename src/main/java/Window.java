f411bdc5a04ea46cbe2364bf4e7ee4589bb616c9
Lokesh0077 <143614284+Lokesh0077@users.noreply.github.com>
2024-11-12 22:53:06 +0530

    Create Window.java

---
+++ src/main/java/Window.java
@@ -0,0 +1,27 @@
+
+public class Window extends Event{
+    public void open() {
+        System.out.println("Opening window");
+        event();
+    }
+
+    public void close() {
+        System.out.println("Closing window");
+        event();  
+    }
+
+    public void move() {
+        System.out.println("Moving window");
+        event();
+    }
+
+    public void display() {
+        System.out.println("Displaying window");
+        event();
+    }
+
+    public void handleEvent(Event event) {
+        System.out.println("Handling event in window");
+        event();
+    }
+}
