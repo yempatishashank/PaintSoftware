c8b58d054897ba546a5a00c70019e7e3aa0601be
HanishaCh <chiruvollanka.hanisha2021@vitstudent.ac.in>
2024-11-12 23:04:56 +0530

    Update DialogBox.java

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -1,6 +1,6 @@
 public class DialogBox extends Window {
     public static void db()
     {
-        System.out.println("Dialouge box is popped");
+        System.out.println("Dialouge box is a popped");
     }
 }
